eab7543386af0b6cf050d09f78d1c0228581a196
SirYavuz <51060721+SirYavuz@users.noreply.github.com>
2020-06-05 21:34:45 +0300

    Initial commit

---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+Locales['en'] = {
+  ['male'] = 'Erkek',
+  ['female'] = 'Kadın',
+  ['carjack'] = 'Araç hırsızlığı devam ediyor:  %s was spotted jacking an %s at %s',
+  ['combat'] = 'Devam eden kavga ihbarı: a %s has been spotted fighting at %s',
+  ['gunshot'] = 'Silahlı saldırı ihbarı ~s~: a %s has been spotted firing a weapon at %s',
+}
